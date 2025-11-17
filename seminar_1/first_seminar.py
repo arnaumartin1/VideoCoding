1a4147e7d10ce8477c4988ae3ba9ded677b34de2
byEriK12 <eric.matasp12@gmail.com>
2025-11-17 18:21:11 +0100

    Ex 2 & 3 finished

--- seminar_1/first_seminar.py
+++ seminar_1/first_seminar.py
@@ -50,42 +50,42 @@ if __name__ == "__main__":
 
     translator = ColorTranslator()
 
-    # # Eercise 1 Test
-    # # Ask user for RGB input values
-    # R_in = int(input("Enter Red component (0-255): "))
-    # G_in = int(input("Enter Green component (0-255): "))
-    # B_in = int(input("Enter Blue component (0-255): "))
+    # Eercise 1 Test
+    # Ask user for RGB input values
+    R_in = int(input("Enter Red component (0-255): "))
+    G_in = int(input("Enter Green component (0-255): "))
+    B_in = int(input("Enter Blue component (0-255): "))
     
-    # print(f"\nRGB to YUV")
-    # print(f"Input RGB: ({R_in}, {G_in}, {B_in})")
+    print(f"\nRGB to YUV")
+    print(f"Input RGB: ({R_in}, {G_in}, {B_in})")
 
-    # # Conversion
-    # Y, U, V = translator.rgb_to_yuv(R_in, G_in, B_in)
-    # print(f"Resulting YUV: ({Y}, {U}, {V})\n")
+    # Conversion
+    Y, U, V = translator.rgb_to_yuv(R_in, G_in, B_in)
+    print(f"Resulting YUV: ({Y}, {U}, {V})\n")
 
-    # # Inverse example:
-    # # Ask user for YUV input values
-    # Y_in = int(input("Enter Y component (0-255): "))
-    # U_in = int(input("Enter U component (0-255): "))
-    # V_in = int(input("Enter V component (0-255): "))
+    # Inverse example:
+    # Ask user for YUV input values
+    Y_in = int(input("Enter Y component (0-255): "))
+    U_in = int(input("Enter U component (0-255): "))
+    V_in = int(input("Enter V component (0-255): "))
 
-    # Y_in, U_in, V_in = Y, U, V
-    # print(f"\nYUV to RGB")
-    # print(f"Input YUV: ({Y_in}, {U_in}, {V_in})")
+    Y_in, U_in, V_in = Y, U, V
+    print(f"\nYUV to RGB")
+    print(f"Input YUV: ({Y_in}, {U_in}, {V_in})")
 
-    # # Inverse Conversion
-    # R_out, G_out, B_out = translator.yuv_to_rgb(Y_in, U_in, V_in)
-    # print(f"Resulting RGB: ({R_out}, {G_out}, {B_out})\n")
+    # Inverse Conversion
+    R_out, G_out, B_out = translator.yuv_to_rgb(Y_in, U_in, V_in)
+    print(f"Resulting RGB: ({R_out}, {G_out}, {B_out})\n")
 
-    # # Comparison:
-    # print(f"\nOriginal RGB: ({R_in}, {G_in}, {B_in})")
-    # print(f"Final RGB:    ({R_out}, {G_out}, {B_out})")
+    # Comparison:
+    print(f"\nOriginal RGB: ({R_in}, {G_in}, {B_in})")
+    print(f"Final RGB:    ({R_out}, {G_out}, {B_out})")
 
 
     # Exercise 2 Test
     
-    input_image_path = "/Users/arnaumartin/VideoCoding/seminar_1/GOAT.jpg"
-    output_image_path = "/Users/arnaumartin/VideoCoding/seminar_1/GOAT_resized.jpg"
+    input_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT.jpg"
+    output_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT_resized.jpg"
     target_width = int(input("Enter target width: "))
     target_height = int(input("Enter target height: "))
     translator.ResizeImages(input_image_path, output_image_path, target_width, target_height)
