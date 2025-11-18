8e6b1f422da5cd3aca330d17dcda6872a61bbe5d
byEriK12 <eric.matasp12@gmail.com>
2025-11-18 16:28:58 +0100

    Ex5 and Ex6

--- seminar_1/first_seminar.py
+++ seminar_1/first_seminar.py
@@ -7,7 +7,7 @@ class ColorTranslator:
 
         pass
 
-    # Exercise 1
+    # Exercise 2
 
     def rgb_to_yuv(self, R: int, G: int, B: int) -> tuple:
 
@@ -32,7 +32,7 @@ class ColorTranslator:
     
     
 
-    # Exercise 2
+    # Exercise 3
 
     def ResizeImages(self, imagePath, outputPath, targetWidth, targetHeight):
 
@@ -42,6 +42,39 @@ class ColorTranslator:
         else:             
             print("Target width and height must be positive integers.")
 
+    
+    # Exercise 5
+
+    def BlackWhiteCompression(self, imagePath, outputPath):
+
+        consoleString = f"ffmpeg -y -i \"{imagePath}\" -vf format=gray -q:v 31 \"{outputPath}\" -loglevel error"
+        subprocess.run(consoleString, shell=True, check=True)
+
+        print("\nBlack and white compression completed.")
+        print("The filter '-vf format=gray' converts the image to grayscale by removing color information.")
+        print("The parameter '-q:v 31' sets the worst possible quality for the output.")
+        print("This leads to a smaller file size and a significant loss of detail.")
+
+
+    # Exercise 6
+
+    def run_length_encoding(self, bytes: bytes) -> list:
+        
+        i = 0
+        encoded_bytes = []
+
+        while i < len(bytes):
+            value = bytes[i]
+            count = 1
+            while i + count < len(bytes) and bytes[i] == bytes[i + count]:
+                count += 1
+
+            encoded_bytes.append((count, value))
+            i += count
+        return encoded_bytes
+            
+        
+
 
 # Main execution
 
@@ -50,7 +83,7 @@ if __name__ == "__main__":
 
     translator = ColorTranslator()
 
-    # Eercise 1 Test
+    # Eercise 2 Test
     # Ask user for RGB input values
     R_in = int(input("Enter Red component (0-255): "))
     G_in = int(input("Enter Green component (0-255): "))
@@ -82,10 +115,29 @@ if __name__ == "__main__":
     print(f"Final RGB:    ({R_out}, {G_out}, {B_out})")
 
 
-    # Exercise 2 Test
-    
+    # Exercise 3 Test
     input_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT.jpg"
     output_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT_resized.jpg"
     target_width = int(input("Enter target width: "))
     target_height = int(input("Enter target height: "))
     translator.ResizeImages(input_image_path, output_image_path, target_width, target_height)
+
+
+    # Exercise 5 Test
+    input_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT.jpg"
+    bw_output_image_path = "/Users/Eric/Downloads/VideoCoding/seminar_1/GOAT_bw_compressed.jpg"
+    translator.BlackWhiteCompression(input_image_path, bw_output_image_path)
+
+
+    # Exercise 6 Test
+    input_data = [255, 255, 255, 0, 0, 0, 0, 128, 128, 128, 128, 128, 255]
+    print(f"Original Data ({len(input_data)} elements):")
+    print(input_data)
+    
+    # Codification
+    rle_encoded = translator.run_length_encoding(input_data)
+    print("\nCodification by RLE:")
+    print(rle_encoded)
+    
+
+    
